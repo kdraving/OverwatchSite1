c014bde027b81008eea38fe7b256504cebd81a0d
kdraving <kdraving@gmail.com>
2015-06-08 12:01:15 -0400

    UploadingSite

---
+++ Overwatch/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace Overwatch.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ Overwatch/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Overwatch.Startup))]
+namespace Overwatch
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
